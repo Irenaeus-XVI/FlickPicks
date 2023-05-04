ee7d75fb1dc3a38b948e82fbf942448c3cbe0f19
Marwan Farrag <marwanayman249@gmail.com>
2023-05-04 20:53:33 +0300

    made a dummy registeration xml to use

--- app/app/src/main/java/com/example/app/LoginActivity.java
+++ app/app/src/main/java/com/example/app/LoginActivity.java
@@ -1,15 +1,16 @@
 package com.example.app;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import org.mindrot.jbcrypt.BCrypt;
+
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_login);
 
 
     }
