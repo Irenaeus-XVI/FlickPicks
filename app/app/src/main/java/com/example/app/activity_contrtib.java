d4820697de4dc3daa131a729bc6d9bdb968b8507
AhmadAbdelsalam <ahmedmohamed11782001@gmail.com>
2023-05-07 04:40:21 +0200

    my final commit

--- app/app/src/main/java/com/example/app/activity_contrtib.java
+++ app/app/src/main/java/com/example/app/activity_contrtib.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class activity_contrtib extends AppCompatActivity {
 
@@ -13,10 +14,18 @@ public class activity_contrtib extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contrtib);
+
+        TextView myTextView = findViewById(R.id.contributor_github);
+        myTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLink(v);
+            }
+        });
     }
     public void openLink(View view) {
         String profile = view.getContentDescription().toString();
-        String url = "github.com/"+profile;
+        String url = "https://github.com/"+profile;
         Intent intent = new Intent(Intent.ACTION_VIEW);
         intent.setData(Uri.parse(url));
         startActivity(intent);
